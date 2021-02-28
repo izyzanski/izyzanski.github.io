901d7792eb2e017e4334907a3ad882d6f9c28728
izyzanski <79787546+izyzanski@users.noreply.github.com>
2021-02-28 10:52:15 +0100

    initial commit

---
+++ precache-manifest.455d189272c925eeb2eca649b06725b5.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c3e89d828f15beaabcdc2bbbb81523",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e5422edd402dcc60f9f",
+    "url": "/static/css/main.ec64274d.chunk.css"
+  },
+  {
+    "revision": "715f3bd104c2e8bc5d02",
+    "url": "/static/js/2.3dc91ef6.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/static/js/2.3dc91ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e5422edd402dcc60f9f",
+    "url": "/static/js/main.76b5d3d1.chunk.js"
+  },
+  {
+    "revision": "c881959c28e06a19e431",
+    "url": "/static/js/runtime-main.17bc2469.js"
+  },
+  {
+    "revision": "0c2e513500faa737581c60fdb16f3913",
+    "url": "/static/media/JakUslyszec.0c2e5135.pdf"
+  },
+  {
+    "revision": "2d3a333929b67d055ddb4d00b1cddf7f",
+    "url": "/static/media/JakUslyszec.2d3a3339.jpeg"
+  },
+  {
+    "revision": "299f8d90bba952bbb6174a19f6f23feb",
+    "url": "/static/media/Lekarstwoczy.299f8d90.pdf"
+  },
+  {
+    "revision": "53c3927d985bea168e4e98179249f50a",
+    "url": "/static/media/Lekarstwoczy.53c3927d.jpeg"
+  },
+  {
+    "revision": "0a4d7c6b9e12d44e7064a379b92ac624",
+    "url": "/static/media/Oddelegowanie.0a4d7c6b.jpeg"
+  },
+  {
+    "revision": "db2d51a50d28c315c89a0dcff2962125",
+    "url": "/static/media/Oddelegowanie.db2d51a5.pdf"
+  },
+  {
+    "revision": "98af8109bef5b1ea86b552dfff67b0f0",
+    "url": "/static/media/Rowneszanse.98af8109.pdf"
+  },
+  {
+    "revision": "af43c2adb4857964352902883e3333b4",
+    "url": "/static/media/Rowneszanse.af43c2ad.jpeg"
+  },
+  {
+    "revision": "3d656062a709ebd5d3ff134fb3089e8c",
+    "url": "/static/media/about.3d656062.pdf"
+  },
+  {
+    "revision": "64afc9ac7c6d962c8188948d082537b2",
+    "url": "/static/media/about.64afc9ac.png"
+  },
+  {
+    "revision": "1fe92b79253c5074dd28feff91542584",
+    "url": "/static/media/ageism.1fe92b79.jpeg"
+  },
+  {
+    "revision": "91dcf8cb848cb5c944b83659724c37af",
+    "url": "/static/media/ageism.91dcf8cb.pdf"
+  },
+  {
+    "revision": "346c73faf4ac7579b27568f43802fde7",
+    "url": "/static/media/art1Img.346c73fa.png"
+  },
+  {
+    "revision": "ecc14c6aa6aabdbef6b5677ff0142681",
+    "url": "/static/media/art2Img.ecc14c6a.png"
+  },
+  {
+    "revision": "e0138a4c753681e627aa4aa514cd207d",
+    "url": "/static/media/art3Img.e0138a4c.png"
+  },
+  {
+    "revision": "0e4ad1731ef97bdbd2db2740be875df4",
+    "url": "/static/media/art4Img.0e4ad173.png"
+  },
+  {
+    "revision": "77a626ca03f660718a1e903cd608922e",
+    "url": "/static/media/art5Img.77a626ca.png"
+  },
+  {
+    "revision": "d1d1b2b7b02f4c86dbef8e2e03dd9192",
+    "url": "/static/media/art6Img.d1d1b2b7.png"
+  },
+  {
+    "revision": "5ab8508137dc09280a03d3bf1d7a8a0d",
+    "url": "/static/media/art7Img.5ab85081.jpg"
+  },
+  {
+    "revision": "5c42f4ca54cb69daa86790f5eeeb5a5b",
+    "url": "/static/media/art8DiscSummary.5c42f4ca.jpg"
+  },
+  {
+    "revision": "256950a0dfe24391fe5d4f3bf8b8d8d5",
+    "url": "/static/media/art8Img.256950a0.jpeg"
+  },
+  {
+    "revision": "4b3fa9fbefddc3ea36b38806aba4c085",
+    "url": "/static/media/art9Img.4b3fa9fb.jpeg"
+  },
+  {
+    "revision": "a3d039e26eb926bd5d36428dfe4ae7e9",
+    "url": "/static/media/art9SituationalLeadership.a3d039e2.jpg"
+  },
+  {
+    "revision": "f61ba23133bf6ca8a2b7b6a7a52e4130",
+    "url": "/static/media/bFruga.f61ba231.png"
+  },
+  {
+    "revision": "734d019b02084b1666bc371305ab9a8f",
+    "url": "/static/media/beginning1.734d019b.jpg"
+  },
+  {
+    "revision": "9a7bb05c01235b2652815bfad7ad39c7",
+    "url": "/static/media/beginning2.9a7bb05c.jpg"
+  },
+  {
+    "revision": "eaea08fc2897d73235bcd276b4ecc3fe",
+    "url": "/static/media/bepractice.eaea08fc.png"
+  },
+  {
+    "revision": "958ea57fbb70c134df2fae773a09dbe3",
+    "url": "/static/media/bss.958ea57f.png"
+  },
+  {
+    "revision": "4b1e1a99b9ecc1fd0d0a2b91ecf56a6c",
+    "url": "/static/media/ccl.4b1e1a99.png"
+  },
+  {
+    "revision": "fa9cbcf84b15bea7da91222294fd3287",
+    "url": "/static/media/coachwise.fa9cbcf8.png"
+  },
+  {
+    "revision": "89e7652116ca1ad9ee440f9e73a0f2e4",
+    "url": "/static/media/cover.89e76521.png"
+  },
+  {
+    "revision": "e24560dee60ea63135c3912a369d0f6e",
+    "url": "/static/media/cover.e24560de.pdf"
+  },
+  {
+    "revision": "db6a58993263ed26174ae1258f111238",
+    "url": "/static/media/eifid.db6a5899.png"
+  },
+  {
+    "revision": "32cef6f56c8da586efad8dec5b10c238",
+    "url": "/static/media/heroBook.32cef6f5.jpg"
+  },
+  {
+    "revision": "c19c3f904fc05bf4c75d261e5ec61b2c",
+    "url": "/static/media/heroHome.c19c3f90.jpg"
+  },
+  {
+    "revision": "e024d48495f5ff0876fd8474ed139949",
+    "url": "/static/media/heroPartners.e024d484.jpg"
+  },
+  {
+    "revision": "7fd141431711682ca321e2ea7ecc3596",
+    "url": "/static/media/heroPublications.7fd14143.jpg"
+  },
+  {
+    "revision": "bfc10b33f89ff256aacaa98af72d9513",
+    "url": "/static/media/heroVideos.bfc10b33.jpg"
+  },
+  {
+    "revision": "25969552712144b087079b809eb755b5",
+    "url": "/static/media/inteligencja.25969552.pdf"
+  },
+  {
+    "revision": "2d3a333929b67d055ddb4d00b1cddf7f",
+    "url": "/static/media/inteligencja.2d3a3339.jpeg"
+  },
+  {
+    "revision": "fb2eb1134b0f3532932b9fd93a6e1c40",
+    "url": "/static/media/irekProfile.fb2eb113.jpg"
+  },
+  {
+    "revision": "0be96c7196468c431c273ea26fa20c0d",
+    "url": "/static/media/videoThumb1.0be96c71.png"
+  },
+  {
+    "revision": "fb5f89b62085a0c8a9a971167b4621c0",
+    "url": "/static/media/videoThumb1.fb5f89b6.png"
+  },
+  {
+    "revision": "643270575e7f8d871dab8dd3ec03e060",
+    "url": "/static/media/videoThumb2.64327057.png"
+  },
+  {
+    "revision": "027a052fab580602a6e6e21997409e52",
+    "url": "/static/media/wirtualnezespolypracy.027a052f.jpeg"
+  },
+  {
+    "revision": "ade4b6f6bcf2eff05376cb0fb380595b",
+    "url": "/static/media/wirtualnezespolypracy.ade4b6f6.pdf"
+  }
+]);
